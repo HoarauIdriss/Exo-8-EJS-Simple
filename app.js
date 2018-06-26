7e6779cef9dcea32e2706c6d2e90eadbaebecbdd
ILLAOI <Idriss.HOARAU@laposte.net>
2018-06-26 09:47:15 +0400

    ok

--- app.js
+++ app.js
@@ -8,26 +8,24 @@ var port = 3333;
 // on utilise le moteur de template ejs
 app.set('view engine', 'ejs');
 
-// créer le service qui permet de récupérer les données de data.json
-app.get("/liste", function (req, res) {
-    res.sendFile(__dirname + "/data.json")
-})
+// création de la fonction qui appel la variable prénom
+function afficheprenom(res)
+
+{
+
+   var prenom = {prenom:"Idriss"};
+
+   res.render('index', 
+    prenom)
+
+}
+
+
+
 // route principale, pour afficher la page d'accueil
 app.get("/", function (req, res) {
-
-    let url = "http://localhost:3333/liste";
-    //script de lecture des données d'url
-    request(url, function (err, response, body) {
-        if (err) { //si erreur, affiche error
-            console.log('error:', error);
-        } else { // si ok, traitement du json pour utilisation via le moteur de template 
-            var renvoialiste = JSON.parse(body)
-            console.log('body:', body);
-        }
-       // envoi des données vers le moteur de template ex: page index qui se trouve dans le dossier view
-        res.render('index', {
-            liste: renvoialiste
-        });
-    });
+afficheprenom(res)
+    
+    
 })
 app.listen(port, () => console.log('Cool mec !'))
